87bc88bcdb2d638b1e925a7cad74a167bbe8e3ee
Dieeh <diego.paz@hotmail.com>
2022-10-13 02:01:41 -0300

    xd

--- INF239/tarea1/server.py
+++ INF239/tarea1/server.py
@@ -1,6 +1,11 @@
 import pyodbc
 
+server = 'DIEGOPC\SQLEXPRESS'
+database = 'MultiUSM' 
+username = 'root' 
+password = 'diego123' 
 
-
+cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+database+';ENCRYPT=yes;UID='+username+';PWD='+ password)
+cursor = cnxn.cursor()
 
 
